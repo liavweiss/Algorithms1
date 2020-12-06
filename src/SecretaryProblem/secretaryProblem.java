eb7700879e813ff71eaf32361ef471ff234807e2
liavweiss <liavweiss@gmail.com>
2020-12-06 20:18:58 +0200

    init

---
+++ src/SecretaryProblem/secretaryProblem.java
@@ -0,0 +1,28 @@
+package SecretaryProblem;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * This department represents the problem of the secretary, the problem is: a certain office gives service
+ * to n clients, the purpose of the secretary is to reduce as much as possible the average time that the clients
+ * are in the office.
+ * We see that the average time is smaller when the other adjacent treatment times are in ascending order.
+ */
+public class secretaryProblem {
+
+    public static Double getAvarageTime(int [] times){
+        Arrays.sort(times);
+        double avg = 0;
+        for(int i : times){
+            avg=avg+avg+i;
+        }
+        return (avg/times.length);
+    }
+
+    public static void main(String[] args) {
+        int [] time = {3 , 4 ,6 ,2 };
+        System.out.println(getAvarageTime(time));
+        System.out.println(Arrays.toString(time));
+    }
+}
