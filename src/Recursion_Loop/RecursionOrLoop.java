a2950e432bebc09c77c8b4077eba8e364118cc13
liavweiss <liavweiss@gmail.com>
2020-12-06 11:25:06 +0200

    init

---
+++ src/Recursion_Loop/RecursionOrLoop.java
@@ -0,0 +1,35 @@
+package Recursion_Loop;
+
+public class RecursionOrLoop {
+
+    //loop is better: loop:O(n) , recursion:O(2^n)
+    public static int [] fibonacci(int n){
+        int [] a = new int [n];
+        a[0]=a[1]=1;
+        for(int i=2; i<n; i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a;
+    }
+
+    //loop is better:because the memory. loop:O(n) , recursion:O(n)
+    public static int factoriarl(int n){
+       int sum=1;
+        for(int i=1; i<n; i++){
+            sum*=i;
+        }
+        return sum;
+    }
+
+    //recursion is better:because it is a readable code. run time:O(n log n).
+    public static int [] mergeSort (int [] a , int low , int high){
+        if(low<high){
+            int mid=(low+high)/2;
+            mergeSort(a,low,mid);
+            mergeSort(a,mid+1,high);
+          //  merge(a[low,mid] , a[mid,high]); //need the merge function.
+        }
+        return a;
+    }
+
+}
