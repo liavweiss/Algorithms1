5fe9064f66d06a3e703fe28dffc40f146154218f
liavweiss <liavweiss@gmail.com>
2021-01-10 23:17:16 +0200

    add factorial in recursion

--- src/Recursion_Loop/RecursionOrLoop.java
+++ src/Recursion_Loop/RecursionOrLoop.java
@@ -13,13 +13,19 @@ public class RecursionOrLoop {
     }
 
     //loop is better:because the memory. loop:O(n) , recursion:O(n)
-    public static int factoriarl(int n){
+    public static int factorialIterative(int n){
        int sum=1;
         for(int i=1; i<n; i++){
             sum*=i;
         }
         return sum;
     }
+
+    public static int factorialRrecursion(int n){
+       if(n==1) {return 1;}
+       return n*factorialRrecursion(n-1);
+    }
+
 
     //recursion is better:because it is a readable code. run time:O(n log n).
     public static int [] mergeSort (int [] a , int low , int high){
@@ -31,4 +37,7 @@ public class RecursionOrLoop {
         }
         return a;
     }
+
 }
+
+
